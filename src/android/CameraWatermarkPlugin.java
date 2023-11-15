c6a4a4091380e041c5d2fa871a70b12d5725fd59
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2023-11-15 12:19:49 +0000

    Update CameraWatermarkPlugin.java

--- src/android/CameraWatermarkPlugin.java
+++ src/android/CameraWatermarkPlugin.java
@@ -7,7 +7,8 @@ import android.view.SurfaceHolder;
 import org.apache.cordova.CordovaInterface; // Add this import statement
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
-import org.json.JSONArray;
+import org.apache.cordova.CallbackContext;
+import org.json.JSONArray; 
 import org.json.JSONException;
 import org.json.JSONObject;
 
